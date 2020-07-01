d9eeeee244e2ace7f6b95e6d1fb256fa5a49ca1a
Mahmoud <mahmoudabdelshafi4@gmail.com>
2020-07-01 22:10:03 +0200

    Initial with MVC

---
+++ Raye7-IOS-Task/Core/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Raye7-IOS-Task
+//
+//  Created by Mahmoud on 7/1/20.
+//  Copyright © 2020 Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+    
+    
+}
+
